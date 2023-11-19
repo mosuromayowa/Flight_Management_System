794b76d4ba29e5a0bb46380bda286da684994302
mosuromayowa <mosuromayowa@gmail.com>
2023-11-19 16:03:57 -0500

    Fields created for passenger class

--- Passenger.java
+++ Passenger.java
@@ -1,3 +1,25 @@
 public class Passenger {
     // Passenger class
+    // fields
+    private int passportId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String age;
+
+    private String flightFare;
+
+    private boolean hasBooked;
+
+    public Passenger (int passportId, String firstName, String lastName, String age, String flightFare,boolean hasBooked) {
+        this.passportId = passportId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.flightFare = flightFare;
+        this.hasBooked = false;
+    }
+
 }
